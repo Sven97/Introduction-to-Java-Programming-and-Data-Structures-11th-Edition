ab764b503fa7dabb009fc07a25588c4b87f7f2dd
Sven <sven.chiu@hotmail.com>
2018-08-17 03:18:26 +0800

    Formatted the code

--- src/Chapter01/Exercise01_05.java
+++ src/Chapter01/Exercise01_05.java
@@ -1,6 +1,6 @@
 /**
  * (Compute expressions) Write a program that displays the result of
- * (9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5).
+ * (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5).
  *
  * Created by Sven on 08/16/18.
  */
