d4ecac3c96d795e93dc6e3a5e7f59a801c490f0d
Sven <sven.chiu@hotmail.com>
2018-09-06 12:33:57 +0800

    Update solutions

---
+++ src/Chapter03/Exercise0335_Even_or_odd_number.java
@@ -0,0 +1,28 @@
+/**
+ * (Even or odd number) Write a program that prompts the user to enter an integer and displays whether the integer is an
+ * odd number or not.
+ *
+ * Here are the sample runs:
+ * Enter an integer: 200
+ * 200 is even number
+ * Enter an integer: 211
+ * 211 is odd number
+ *
+ * Created by Sven on 9/6/2018.
+ */
+package Chapter03;
+
+import java.util.Scanner;
+
+public class Exercise0335_Even_or_odd_number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number");
+        } else {
+            System.out.println(number + " is odd number");
+        }
+    }
+}
