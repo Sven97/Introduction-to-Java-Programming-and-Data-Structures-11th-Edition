b772e9437c9a30e2af390f18111c94551a1fa070
mustafa335 <mustafashift@hotmail.com>
2020-05-24 07:52:40 +0200

    Update Exercise0206_Sum_the_digits_in_an_integer.java
    The number should be only between 0 and 1000, with your solution I can enter any number even if it is greater than 1000 or less than 0 an get the multiplication

--- src/Chapter02/Exercise0206_Sum_the_digits_in_an_integer.java
+++ src/Chapter02/Exercise0206_Sum_the_digits_in_an_integer.java
@@ -18,16 +18,24 @@ import java.util.Scanner;
 public class Exercise0206_Sum_the_digits_in_an_integer {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number between 0 and 1000: ");
+
+        System.out.print("Enter an integer number between 0 and 1000: ");
         int initialNumber = input.nextInt();
 
         int number = initialNumber;
         int multiplication = initialNumber % 10;
+        
+        if (initialNumber > 1000 || initialNumber < 0) {
+            System.out.println("Out of range!");
+            System.out.println("Number should be between 0 & 1000!");
+        } else {
+            while (number / 10 != 0) {
+                number = number / 10;
+                multiplication *= number % 10;
+            }
 
-        while (number / 10 != 0) {
-            number = number / 10;
-            multiplication *= number % 10;
+            System.out.println("Multiplication of the digits in the number "
+                    + initialNumber + " equals to " + multiplication);
         }
-        System.out.println("The multiplication of all digits in " + initialNumber + " is " + multiplication);
     }
 }
